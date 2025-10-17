786f3458135ca51ca0b29cb96267ef7a7232d7a1
yvbbrjdr <xyyqzy@hotmail.com>
2025-10-17 00:52:39 -0700

    spark

--- python/triton_kernels/triton_kernels/matmul_ogs_details/_p_matmul_ogs.py
+++ python/triton_kernels/triton_kernels/matmul_ogs_details/_p_matmul_ogs.py
@@ -372,6 +372,9 @@ def _p_matmul_ogs(
         else:
             w_scale = load_scale(WScale)
 
+        if SWAP_XW:
+            acc = acc.T
+
         accs = (acc,)
         biases = (bias,)
 
@@ -395,9 +398,6 @@ def _p_matmul_ogs(
         for a_i in tl.static_range(len(accs)):
             acc_tile = accs[a_i]
             acc_tile *= x_scale * w_scale
-
-            if SWAP_XW:
-                acc_tile = acc_tile.T
 
             acc_tile = acc_tile + biases[a_i][None, :] * betas[:, None]
             if out_alpha is not None:
--- python/triton_kernels/triton_kernels/matmul_ogs_details/opt_flags.py
+++ python/triton_kernels/triton_kernels/matmul_ogs_details/opt_flags.py
@@ -2,7 +2,7 @@
 # fmt: off
 from dataclasses import dataclass
 import triton
-from triton_kernels.target_info import get_cdna_version
+from triton_kernels.target_info import get_cdna_version, has_native_mxfp
 import torch
 from .opt_flags_details import opt_flags_amd, opt_flags_nvidia
 
@@ -176,7 +176,9 @@ def make_default_opt_flags_nvidia(
         has_simple_epilogue = precision_config.max_num_imprecise_acc is None
         is_persistent = supports_persistent and has_simple_epilogue and (tiles_per_sm >= 2.0 or lhs_dtype.itemsize <= 1) and out_dtype.itemsize < 4
         # TEMP CHANGE
-        if precision_config.act_scale is not None or precision_config.out_scale is not None:
+        if has_native_mxfp() and precision_config.weight_scale is not None:
+            is_persistent = True
+        elif precision_config.act_scale is not None or precision_config.out_scale is not None:
             is_persistent = False
     # block k
     if constraints.get("block_k", None) is not None:
@@ -187,6 +189,8 @@ def make_default_opt_flags_nvidia(
     if constraints.get("split_k", None) is not None:
         split_k = constraints["split_k"]
     elif is_persistent or enforce_bitwise_invariance or precision_config.act_scale is not None or precision_config.out_scale is not None:
+        split_k = 1
+    elif has_native_mxfp() and precision_config.weight_scale is not None:
         split_k = 1
     else:
         estimated_actual_grid_size = opt_flags_nvidia.compute_grid_size(None, m, n, block_m, block_n)
